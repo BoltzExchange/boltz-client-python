eb5d6589169fcc182ecc9c1c697f37505642f328
dni ⚡ <office@dnilabs.com>
2023-11-06 16:16:37 +0100

    add tests

---
+++ tests/test_onchain.py
@@ -0,0 +1,31 @@
+import pytest
+
+from boltz_client.onchain import validate_address
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "addr, network",
+    [
+        ("bc1asdkljalskdj", "main"),
+        ("bc1qar0srrr7xfkvy5l643lydnw9re59gtzzwf5mdq", "test"),
+        ("bc1qar0srrr7xfkvy5l643lydnw9re59gtzzwf5mdq", "regtest"),
+        ("bcrt1qar0srrr7xfkvy5l643lydnw9re59gtzzwf5mdq", "regtest"),
+        ("026165850492521f4ac8abd9bd8088123446d126f648ca35e60f88177dc149ceb2", "main"),
+    ],
+)
+async def test_invalid_address(addr, network):
+    with pytest.raises(ValueError):
+        validate_address(addr, network)
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "addr, network",
+    [
+        ("bc1qar0srrr7xfkvy5l643lydnw9re59gtzzwf5mdq", "main"),
+        ("bcrt1qky0es27zfejlr3grpfl4pj47w7yfm0atwqdf3y", "regtest"),
+    ],
+)
+async def test_valid_address(addr, network):
+    validate_address(addr, network)
