0827d5f05c5ca99d50b569e4049b765dfaed285d
jgriffiths <jon_p_griffiths@yahoo.com>
2023-11-24 07:47:27 +1300

    Fix psbt_wally.py (#16)
    * psbt_wally: don't fail if output 0 matches the lockup script
    * psbt_wally: strip trailing newlines etc from input files
    * psbt_wally: fix asset_unblind return assignments
    * psbt_wally: various fixes to make the script work
    - Fix input values for a p2wsh input
    - Add a keypath and use wally to sign
    - Correctly finalize the input

--- psbt_wally.py
+++ psbt_wally.py
@@ -17,18 +17,11 @@ LASSET = bytes.fromhex(
 
 
 def set_blinding_data(idx, utxo, values, vbfs, assets, abfs):
-    wally.map_add_integer(
-        values, idx, wally.tx_confidential_value_from_satoshi(utxo["satoshi"])
-    )
+    value = wally.tx_confidential_value_from_satoshi(utxo["satoshi"])
+    wally.map_add_integer(values, idx, value)
     wally.map_add_integer(vbfs, idx, utxo["amountblinder"])
     wally.map_add_integer(assets, idx, utxo["asset_id"])
     wally.map_add_integer(abfs, idx, utxo["assetblinder"])
-
-
-def get_blinding_nonce(psbt, ephemeral_keys, output_index):
-    ephemeral_key = wally.map_get_item(ephemeral_keys, 0)
-    blinding_pubkey = wally.psbt_get_output_blinding_public_key(psbt, output_index)
-    return wally.ecdh_nonce_hash(blinding_pubkey, ephemeral_key)
 
 
 def get_entropy(num_outputs_to_blind):
@@ -43,11 +36,10 @@ def get_entropy(num_outputs_to_blind):
 
 # INIT
 with open("./reverse_swap.json") as f:
-    swap_json = f.read()
+    reverse_swap = json.loads(f.read().strip())
 with open("./lockup_rawtx.txt") as f:
-    lockup_rawtx = f.read()
+    lockup_rawtx = f.read().strip()
 
-reverse_swap = json.loads(swap_json)
 redeem_script = bytes.fromhex(reverse_swap["redeem_script_hex"])
 preimage = bytes.fromhex(reverse_swap["preimage_hex"])
 private_key = wally.wif_to_bytes(
@@ -81,7 +73,7 @@ receive_script_pubkey = wally.addr_segwit_to_bytes(
 
 
 # parse lockup tx
-lockup_tx = wally.tx_from_hex(hex=lockup_rawtx, flags=wally.WALLY_TX_FLAG_USE_ELEMENTS)
+lockup_tx = wally.tx_from_hex(lockup_rawtx, wally.WALLY_TX_FLAG_USE_ELEMENTS)
 vout_n: int | None = None
 for vout in range(wally.tx_get_num_outputs(lockup_tx)):
     script_out = wally.tx_get_output_script(lockup_tx, vout)
@@ -89,7 +81,7 @@ for vout in range(wally.tx_get_num_outputs(lockup_tx)):
         vout_n = vout
         break
 
-assert vout_n, "Lockup vout not found"
+assert vout_n is not None, "Lockup vout not found"
 
 txid = wally.tx_get_txid(lockup_tx)
 lockup_script = wally.tx_get_output_script(lockup_tx, vout_n)
@@ -99,7 +91,7 @@ lockup_asset_commitment = wally.tx_get_output_asset(lockup_tx, vout_n)
 lockup_value_commitment = wally.tx_get_output_value(lockup_tx, vout_n)
 
 # UNBLIND
-unblinded_amount, unblinded_asset, abf, vbf = wally.asset_unblind(
+unblinded_amount, unblinded_asset, vbf, abf = wally.asset_unblind(
     lockup_ephemeral_pubkey,
     blinding_key,
     lockup_rangeproof,
@@ -116,46 +108,66 @@ utxo = {
 
 assert unblinded_asset == LASSET, "Wrong asset"
 
-
-# PSBT
+# INITIALIZE PSBT (PSET)
 num_vin = 1
 num_vout = 2
-psbt = wally.psbt_init(
-    wally.WALLY_PSBT_VERSION_2, num_vin, num_vout, 0, wally.WALLY_PSBT_INIT_PSET
-)
+psbt_flags = wally.WALLY_PSBT_INIT_PSET  # Make an Elements PSET
+psbt_version = wally.WALLY_PSBT_VERSION_2  # PSET only supports v2
+psbt = wally.psbt_init(psbt_version, num_vin, num_vout, 0, psbt_flags)
 
-# INPUT
+
+# ADD PSBT INPUT
 idx = wally.psbt_get_num_inputs(psbt)
+# Add the txout from the lockup tx as the witness UTXO for our input
 seq = 0xFFFFFFFE  # RBF not enabled for liquid yet
-input = wally.tx_input_init(txid, vout_n, seq, None, None)
-wally.psbt_add_tx_input_at(psbt, idx, 0, input)
+input_ = wally.tx_input_init(txid, vout_n, seq, None, None)
+wally.psbt_add_tx_input_at(psbt, idx, 0, input_)
 wally.psbt_set_input_witness_utxo_from_tx(psbt, idx, lockup_tx, vout_n)
+# Add the rangeproof
 wally.psbt_set_input_utxo_rangeproof(psbt, idx, lockup_rangeproof)
-wally.psbt_set_input_witness_script(psbt, idx, lockup_script)
-wally.psbt_set_input_redeem_script(psbt, idx, redeem_script)
-wally.psbt_set_input_amount_rangeproof(psbt, idx, lockup_rangeproof)
+# And the witness script
+wally.psbt_set_input_witness_script(psbt, idx, redeem_script)
+# Add the key info for our private key, so psbt_sign knows what input
+# to sign when given the private key.
+# Since we don't have a BIP32 key, add it with a dummy fingerprint and path.
+# When signing with a non-BIP32 private key, wally uses the key as given
+# and doesn't attempt to derive a BIP32 key to sign with, so these dummy
+# values aren't used except to indicate that the key belongs to this input.
+keypaths = wally.map_keypath_public_key_init(1)
+signing_pubkey = wally.ec_public_key_from_private_key(private_key)
+wally.map_keypath_add(keypaths, signing_pubkey, bytes(4), [0])
+wally.psbt_set_input_keypaths(psbt, idx, keypaths)
+# Uncomment to generate explicit value proofs for the input.
+# These expose the unblinded value and asset in the PSBT; we
+# don't need them for this use-case.
+if False:
+    wally.psbt_generate_input_explicit_proofs(psbt, idx,
+                                              utxo["satoshi"],
+                                              utxo["asset_id"],
+                                              utxo["assetblinder"],
+                                              utxo["amountblinder"],
+                                              secrets.token_bytes(32))
 
-wally.psbt_generate_input_explicit_proofs(psbt, idx, utxo['satoshi'], utxo["asset_id"], utxo["assetblinder"], utxo["amountblinder"], secrets.token_bytes(32))
 
-
-
-# OUTPUT
+# ADD PSBT OUTPUT
 output_idx = wally.psbt_get_num_outputs(psbt)
-asset_tag = bytearray([1]) + unblinded_asset
-
+asset_tag = bytearray([1]) + unblinded_asset  # Explicit (unblinded) asset
 value = wally.tx_confidential_value_from_satoshi(unblinded_amount - FEE)
-txout = wally.tx_elements_output_init(receive_script_pubkey, asset_tag, value, None)
+txout = wally.tx_elements_output_init(receive_script_pubkey, asset_tag,
+                                      value, None)
 wally.psbt_add_tx_output_at(psbt, output_idx, 0, txout)
-wally.psbt_set_output_blinding_public_key(psbt, output_idx, receive_blinding_pubkey)
+wally.psbt_set_output_blinding_public_key(psbt, output_idx,
+                                          receive_blinding_pubkey)
 wally.psbt_set_output_blinder_index(psbt, output_idx, 0)
 
-# Add the fee output
+
+# ADD FEE OUTPUT
 fee_value = wally.tx_confidential_value_from_satoshi(FEE)
 fee_txout = wally.tx_elements_output_init(None, asset_tag, fee_value)
 wally.psbt_add_tx_output_at(psbt, output_idx + 1, 0, fee_txout)
 
 
-# BLINDING
+# BLIND PSBT
 entropy = get_entropy(1)
 values, vbfs, assets, abfs = [wally.map_init(1, None) for _ in range(4)]
 set_blinding_data(0, utxo, values, vbfs, assets, abfs)
@@ -163,35 +175,40 @@ set_blinding_data(0, utxo, values, vbfs, assets, abfs)
 ephemeral_keys = wally.psbt_blind(
     psbt, values, vbfs, assets, abfs, entropy, output_idx, 0
 )
-# nonce = get_blinding_nonce(psbt, ephemeral_keys, output_idx)
 
-
-
-# signing
+# SIGN PSBT
+# wally can identify the input to sign because we gave the keypath above
 wally.psbt_sign(psbt, private_key, wally.EC_FLAG_ECDSA)
+# Fetch the signature from the PSBT input for finalization
+sig_pos = wally.psbt_find_input_signature(psbt, idx, signing_pubkey)
+assert sig_pos != 0, 'signature not found'
+sig = wally.psbt_get_input_signature(psbt, idx, sig_pos - 1)
 
-# Finalize
-wally.psbt_finalize_input(psbt, idx, 0)
-wally.psbt_finalize(psbt, 0)
+# FINALIZE PSBT
+# Wally cant't know how to finalize our bespoke p2wsh input, so
+# we do it manually:
+# 1) Set the final_witness according to our script requirements
+stack = wally.tx_witness_stack_init(3)
+wally.tx_witness_stack_add(stack, sig)
+wally.tx_witness_stack_add(stack, preimage)
+wally.tx_witness_stack_add(stack, redeem_script)
+wally.psbt_set_input_final_witness(psbt, idx, stack)
+# 2) Set the final_scriptsig. For p2wsh this must be empty, so
+#    we don't have to do anything.
 
-tx = wally.psbt_extract(psbt, 1)
+# OUTPUT FINALIZED PSBT/TX
+# Convert the PSBT to base64, then parse in strict mode.
+# This uses wally to perform strict verification that everything is OK.
+base64 = wally.psbt_to_base64(psbt, 0)
+wally.psbt_from_base64(base64, wally.WALLY_PSBT_PARSE_FLAG_STRICT)
 
-# signing
-hash = wally.tx_get_elements_signature_hash(
-    tx,
-    0,
-    lockup_script_pubkey,
-    wally.tx_confidential_value_from_satoshi(unblinded_amount),
-    wally.WALLY_SIGHASH_ALL,
-    wally.WALLY_TX_FLAG_USE_WITNESS,
-)
-sig = wally.ec_sig_from_bytes(private_key, hash, wally.EC_FLAG_ECDSA)
-witness_script = wally.tx_witness_stack_init(3)
-wally.tx_witness_stack_add(witness_script, sig)
-wally.tx_witness_stack_add(witness_script, preimage)
-wally.tx_witness_stack_add(witness_script, redeem_script)
-wally.tx_set_input_witness(tx, 0, witness_script)
+# Extract the completed tx from the now-finalized psbt
+tx = wally.psbt_extract(psbt, 0)  # 0 == must be finalized
 
-tx_hex = wally.tx_to_hex(tx, wally.WALLY_TX_FLAG_USE_WITNESS)
-#print(tx_hex)
-print(wally.psbt_to_base64(psbt, wally.WALLY_PSBT_INIT_PSET))
+if False:
+    # Dump the psbt. To extract the finalized tx, use e.g:
+    # elements-cli-sim finalizepsbt $(python psbt_wally.py) true
+    print(base64)
+else:
+    # Dump the finalized tx hex ready for broadcasting to the network
+    print(wally.tx_to_hex(tx, wally.WALLY_TX_FLAG_USE_WITNESS))
